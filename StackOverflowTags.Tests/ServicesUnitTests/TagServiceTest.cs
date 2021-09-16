7e62a8d8dfda745ec7e31f82568dbb7eb0b7042a
Adam Halicki <adamhalicki5@gmail.com>
2021-09-16 09:45:13 +0200

    added unit tests

---
+++ StackOverflowTags.Tests/ServicesUnitTests/TagServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Application.Services.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StackOverflowTags.Tests.ServicesUnitTests
+{
+    public class TagServiceTest
+    {
+        [Fact]
+        public async Task GetAllTagsAsync_Is1000Records()
+        {
+            TagService tagService = new TagService();
+            var response = await tagService.GetAllTagsAsync();
+            Assert.Equal(1000, response.Items.Count());
+        }
+    }
+}
